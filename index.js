a740fece632f8dce8e9c1db3b8367c0068615be1
Your Name <teamherapheri1@gmail.com>
2025-09-04 20:26:13 +0530

    Move index.js into /api directory for Vercel

--- index.js
+++
@@ -1,7 +0,0 @@
-// index.js
-module.exports = (req, res) => {
-  res.status(200).json({
-    message: "Print Backend is running successfully!",
-    endpoints: ["/api/create-order", "/api/order-status", "/api/refund"]
-  });
-};
